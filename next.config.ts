7d00b2de97f794a81fa8e8a01d93b9801975bb0d
guilhermepama <guilhermepama1@gmail.com>
2025-02-05 16:11:00 -0300

    Refatoração do Head componente nextImage

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,21 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**/*",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
